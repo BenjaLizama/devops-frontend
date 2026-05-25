160b8b343687c4b412044df4fd17454a6963200c
lucciano martinez <zorrapeluahihu@gmail.com>
2026-05-25 19:35:03 -0400

    config: Configura URLs de backend para build del frontend

--- src/constants/ec2_ip.ts
+++ src/constants/ec2_ip.ts
@@ -1,2 +1,4 @@
-export const API_VENTA_BASE_URL = import.meta.env.VITE_API_VENTA_BASE_URL;
-export const API_DESPACHO_BASE_URL = import.meta.env.VITE_API_DESPACHO_BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export const API_VENTA = `${API_BASE_URL}/api/v1/ventas`;
+export const API_DESPACHO = `${API_BASE_URL}/api/v1/despachos`;
